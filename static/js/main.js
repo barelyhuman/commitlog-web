5efa799c0b1b48badac549105739f7816d1cd803
Reaper Gelera <sidgroks@gmail.com>
2021-03-08 07:59:34 +0530

    chore: use themer instead

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 (function () {
   const themeToggle = document.getElementById('theme-toggle');
-  new Themer({ toggleTrigger: themeToggle });
+  new Themer({ trigger: themeToggle });
 
   const jsElements = document.querySelectorAll('.needs-js');
   jsElements.forEach((item) => {
