3967709da11a7aa8f999d12cf0a99b564d94927f
Siddharth <ahoy@barelyhuman.dev>
2021-06-07 18:56:09 +0530

    chore: retheme

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  const themeToggle = document.getElementById('theme-toggle');
-  new Themer({ trigger: themeToggle });
-
-  const jsElements = document.querySelectorAll('.needs-js');
-  jsElements.forEach((item) => {
-    item.style.visibility = 'visible';
-  });
-})();
